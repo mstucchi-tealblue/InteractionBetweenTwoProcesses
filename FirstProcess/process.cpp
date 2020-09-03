a3427176082809342bddc9745e516ad988b6fc49
mstucchi-tealblue <68645245+mstucchi-tealblue@users.noreply.github.com>
2020-09-03 17:54:58 +0200

    Fixed: Use of startDetached

--- FirstProcess/process.cpp
+++ FirstProcess/process.cpp
@@ -9,12 +9,16 @@ void Process::startProcessFromQml()
 {
     QObject *parent = 0;
 
-    QString program = "C:\\Users\\Matteo\\Documents\\QtProjects\\build-SecondProcess-Desktop_Qt_5_15_0_MSVC2019_64bit-Release\\SecondProcess.exe";
     QStringList arguments;
-        arguments << "-style" << "fusion";
+    arguments << "-style" << "fusion";
 
     process = new QProcess(parent);
-    process->startDetached(program);
+
+    process->setProgram("C:\\Users\\Matteo\\Documents\\QtProjects\\build-SecondProcess-Desktop_Qt_5_15_0_MSVC2019_64bit-Release\\SecondProcess.exe");
+    // process->setArguments({"Jeopardy_Theme.mp3"});
+
+    qint64 pid;
+    process->startDetached(&pid);
 
 }
 
